fbaecea912ef7ef6434946ac9bb3858d9eede236
MarcosNunes <marcosrenatovnunes@hotmail.com>
2025-02-28 14:46:56 -0300

    Revert "Marcos Nunes Test"
    This reverts commit db4fc8da2b2f22a9070e85f18f154e1dddb713b8.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import CatList from './CatList';
-import "./index.css"
-import categories from "./categories";
-
-
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="flex flex-col gap-3 justify-center mt-10">
+    <div className="flex justify-center mt-10">
       <h1 className="text-3xl font-bold underline">Call Suggestions</h1>
       {/* Your solution here */}
-      <CatList categories={categories} />
     </div>
   </StrictMode>
 );
